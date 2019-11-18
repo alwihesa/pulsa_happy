fd2c139377e3b97791ac5e916708e9451e7e955b
alwi <allwemuhammad@gmail.com>
2019-11-18 21:51:56 +0800

    Update Fix Fitur

--- app/src/main/java/com/example/kios_happy/PulsaBiasa.kt
+++ app/src/main/java/com/example/kios_happy/PulsaBiasa.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_pulsa_biasa.*
 import android.content.Intent
 import android.net.Uri
+import androidx.appcompat.app.AlertDialog
 
 
 class PulsaBiasa : AppCompatActivity() {
@@ -18,17 +19,42 @@ class PulsaBiasa : AppCompatActivity() {
         btn_kirim_pls_biasa.setOnClickListener {
             val nomorTujuan = ed_nmr_tlpn.text.toString()
             val jumlahPulsa = ed_pulsa.text.toString()
-            if(nomorTujuan.isNullOrEmpty()){
-                applicationContext.toast("nomor telpon atau pulsa kosong!")
+            if(nomorTujuan.isEmpty()){
+                applicationContext.toast("Nomor telpon atau pulsa kosong!")
             }else {
                 kirimPulsa(nomorTujuan, jumlahPulsa)
+
+                // Initialize a new instance of
+                val builder = AlertDialog.Builder(this)
+
+                builder.setCancelable(false)
+
+                // Set the alert dialog title
+                builder.setTitle("PEMBELIAN PULSA DIPROSES!")
+
+                // Display a message on alert dialog
+                builder.setMessage("Pembelian pulsa sedang diproses, cek SMS untuk informasi lebih lanjut!")
+
+                // Set a positive button and its click listener on alert dialog
+                builder.setPositiveButton("SELESAI"){dialog, which ->
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+
+                // Finally, make the alert dialog using builder
+                val dialog: AlertDialog = builder.create()
+
+                // Display the alert dialog on app interface
+                dialog.show()
             }
+
+
         }
     }
 
     private fun kirimPulsa(nomorTujuan: String, jumlahPulsa: String){
         val kirim = "tel:" + "*777*" + nomorTujuan + "*"+jumlahPulsa+"*999"+Uri.encode("#")
-        val intent = Intent(Intent.ACTION_CALL);
+        val intent = Intent(Intent.ACTION_CALL)
         intent.data = Uri.parse(kirim)
         startActivity(intent)
     }
--- app/src/main/java/com/example/kios_happy/Token.kt
+++ app/src/main/java/com/example/kios_happy/Token.kt
@@ -2,67 +2,84 @@ package com.example.kios_happy
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.SmsManager
+import android.widget.RadioButton
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_token.*
 import java.lang.Exception
 
 class Token : AppCompatActivity() {
-    private var tokenValue = "20"
-    private var destinationNumber : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_token)
 
-        rg_pulsa.setOnCheckedChangeListener { group, checkedId ->
-            if (checkedId == 0) {
-                tokenValue = "" +getString(R.string.pls_20)
-                applicationContext.toast(tokenValue)}
-            if (checkedId == 1){
-                tokenValue = "" +getString(R.string.pls_50)
-                applicationContext.toast(tokenValue)}
-            if (checkedId == 2){
-                tokenValue = "" +getString(R.string.pls_100)
-                applicationContext.toast(tokenValue)}
-            if (checkedId == 3){
-                tokenValue = "" +getString(R.string.pls_200)
-                applicationContext.toast(tokenValue)}
-        }
-
-        rg_nmr.setOnCheckedChangeListener { _, checkedId ->
-            destinationNumber = " " + getString(if (checkedId == 0) R.string.nmr1 else R.string.nmr2)
-            applicationContext.toast(destinationNumber)
-        }
-
         btn_kirim2.setOnClickListener {
-            sendSMS2(destinationNumber, tokenValue, tokenValue)
+            val noToken = ed_nmr_token.text.toString()
+            if(noToken.isNullOrEmpty()){
+                applicationContext.toast("Nomor Token Kosong!")
+            }else {
+                val jumlahTokenDibeli = findViewById<RadioButton>(rg_pulsa.checkedRadioButtonId)
+                val destinationNumber = findViewById<RadioButton>(rg_nmr.checkedRadioButtonId)
+                sendSMS2(
+                    destinationNumber.text.toString(),
+                    jumlahTokenDibeli.text.toString(),
+                    noToken
+                )
+            }
         }
-
     }
 
     private fun Context.toast(message: String){
         Toast.makeText(this,message, Toast.LENGTH_LONG).show() }
 
-    private fun sendSMS(sendTo: String, tokenValue: String, pulsaValue: String) {
-        try {
-            val uri = Uri.parse("smsto:$sendTo")
-            val intent = Intent(Intent.ACTION_SENDTO, uri)
-            intent.putExtra("sms_body", "$tokenValue + $pulsaValue")
-            startActivity(intent)
-            applicationContext.toast("SMS berhasil dikirim!")
-        } catch (e: Exception) {
-            applicationContext.toast("SMS gagal dikirim!")
+
+    private fun sendSMS2 (sendTo: String, tokenValue: String, userToken: String) {
+        try{
+            val smsManager = SmsManager.getDefault() as SmsManager
+            smsManager.sendTextMessage(sendTo, null, "$userToken $tokenValue dong mbak", null, null)
+
+            // Initialize a new instance of
+            val builder = AlertDialog.Builder(this)
+
+            builder.setCancelable(false)
+
+            // Set the alert dialog title
+            builder.setTitle("PEMBELIAN TOKEN DIPROSES!")
+
+            // Display a message on alert dialog
+            builder.setMessage("Pembelian Token sedang diproses, cek SMS untuk informasi lebih lanjut!")
+
+            // Set a positive button and its click listener on alert dialog
+            builder.setPositiveButton("SELESAI"){dialog, which ->
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            // Finally, make the alert dialog using builder
+            val dialog: AlertDialog = builder.create()
+
+            // Display the alert dialog on app interface
+            dialog.show()
+        }catch (e:Exception) {
+            val builder = AlertDialog.Builder(this)
+
+
+            // Set the alert dialog title
+            builder.setTitle("PEMBELIAN TOKEN GAGAL!")
+
+            // Set a positive button and its click listener on alert dialog
+            builder.setPositiveButton("SELESAI"){dialog, which ->
+            }
+
+            // Finally, make the alert dialog using builder
+            val dialog: AlertDialog = builder.create()
+
+            // Display the alert dialog on app interface
+            dialog.show()
         }
-
-    }
-
-    private fun sendSMS2 (sendTo: String, tokenValue: String, pulsaValue: String) {
-        val smsManager = SmsManager.getDefault() as SmsManager
-        smsManager.sendTextMessage(sendTo, null, "$tokenValue, $pulsaValue dong mbak", null, null)
-        applicationContext.toast("SMS berhasil dikirim!")
     }
 }
